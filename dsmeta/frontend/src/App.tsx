954b2964cbbf897f2c6b87220fbdd26f6a3125d7
pedro lucas <peedroo3133@outlook.com>
2022-07-14 13:23:11 -0300

    first component

--- dsmeta/frontend/src/App.tsx
+++ dsmeta/frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import NotificationButton from './componets/NotificationButton'
 function App() {
   return (
+    <>
     <h1>shaudrin</h1>
+   <NotificationButton />
+   <NotificationButton />
+   <NotificationButton />
+   <NotificationButton />
+   <NotificationButton />
+   
+</>
+
   )
 }
-  
-  export default App
 
+export default App
