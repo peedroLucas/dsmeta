fb8ca044de0ca1bd02c64590d9a44a12f5c00e0f
pedro lucas <peedroo3133@outlook.com>
2022-07-15 11:31:29 -0300

    Date select

--- dsmeta/backend/src/main/java/com/pedrosuperior/dsmeta/services/SaleService.java
+++ dsmeta/backend/src/main/java/com/pedrosuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,12 @@
 package com.pedrosuperior.dsmeta.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.pedrosuperior.dsmeta.entities.Sale;
@@ -10,11 +14,17 @@ import com.pedrosuperior.dsmeta.repositories.SaleRepository;
 
 @Service
 public class SaleService {
-	
-    @Autowired
+
+	@Autowired
 	private SaleRepository repository;
-	
-	public List<Sale> findSales() {
-		 return repository.findAll();
+
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+		LocalDate min = minDate.equals("")? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("")? today : LocalDate.parse(maxDate);
+
+		return repository.findSales(min, max, pageable);
 	}
 }
