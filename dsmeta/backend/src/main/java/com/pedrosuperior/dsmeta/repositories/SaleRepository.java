0323498783bc917e9b86c7fa77f44349a725f835
pedro lucas <peedroo3133@outlook.com>
2022-07-15 10:53:50 -0300

    API test

---
+++ dsmeta/backend/src/main/java/com/pedrosuperior/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.pedrosuperior.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pedrosuperior.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
